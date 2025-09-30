cccccdcb4dced9112bfab2776013f45ee1c1b91e
Miguel Rodriguez Villarroel <a2023110971@estudiantes.upsa.edu.bo>
2025-09-29 22:04:10 -0400

    subiendo archivos

---
+++ PrimerParciaL1/Models/Product.cs
@@ -0,0 +1,13 @@
+namespace PrimerParciaL1.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public bool IsActive { get; set; } = true;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; } 
+}
--- PrimerParciaL1/Program.cs
+++ PrimerParciaL1/Program.cs
@@ -1,17 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using PrimerParciaL1.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(connectionString));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
